0e4cee417bc6a8ae0475177cb03fe767499c6ab4
qantasmz <qanta_shimizu@qanta-mac-2.local>
2015-07-08 12:17:15 -0400

    bugfix

--- js/alreadyactivated.js
+++ js/alreadyactivated.js
@@ -2,6 +2,8 @@ var _num = document.URL.split("?num=")[1];
 if(!_num){
 	
 	document.getElementById("desc").innerHTML = 'Error! Please restart from the top page.';
+}else if(_num==1){
+	document.getElementById("desc").innerHTML =  'It looks like your account already has Goodbye Unfollower installed. Your next unfollower will receive a Goodbye Poem from you.';
 }else{
-	document.getElementById("desc").innerHTML = ' It looks like your account already has Goodbye Unfollower installed.The following '+_num+' unfollowers will receive a Goodbye Poem from you.';
+	document.getElementById("desc").innerHTML = ' It looks like your account already has Goodbye Unfollower installed. The following '+_num+' unfollowers will receive a Goodbye Poem from you.';
 }
